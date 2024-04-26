ee8692d3aa33d65e6fc4d3585733ace10b74fea6
Meet Nayak <125037747+meetnayakjain@users.noreply.github.com>
2024-04-26 06:29:13 +0530

    Create quick.py

---
+++ quick.py
@@ -0,0 +1,28 @@
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
+def quicksort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quicksort(array, low, pi - 1)
+        quicksort(array, pi + 1, high)
+        # Driver code
+if __name__ == '__main__':
+    array = []
+     n = int(input("Enter number of elements : "))
+     for i in range(0, n):
+         ele = int(input('Enter element'))
+         array.append(ele)
+    N = len(array)
+
+    # Function call
+    quicksort(array, 0, N - 1)
+    print('Sorted array:')
+    for x in array:
+        print(x, end=" ")
